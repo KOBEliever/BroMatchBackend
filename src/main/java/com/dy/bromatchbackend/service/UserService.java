43e865d6f85ae6236ed8bd69dd4f0a29cb25dee5
一 丁 <970102437@qq.com>
2023-07-10 23:58:13 +0800

    user注册

--- src/main/java/com/dy/bromatchbackend/service/UserService.java
+++ src/main/java/com/dy/bromatchbackend/service/UserService.java
@@ -3,11 +3,36 @@ package com.dy.bromatchbackend.service;
 import com.dy.bromatchbackend.model.domain.User;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
 * @author LEGION
 * @description 针对表【user(用户表)】的数据库操作Service
 * @createDate 2023-07-10 22:32:11
 */
 public interface UserService extends IService<User> {
+    /**
+     * 用户注册
+     * @param userAccount 用户账户
+     * @param userPassword 用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
 
+    /**
+     * 用户登录
+     * @param userAccount 用户账号
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    User userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    /**
+     * 用户脱敏
+     * @param user
+     * @return
+     */
+    User getSaftyUser(User user);
 }
--- src/main/java/com/dy/bromatchbackend/service/impl/UserServiceImpl.java
+++ src/main/java/com/dy/bromatchbackend/service/impl/UserServiceImpl.java
@@ -1,10 +1,17 @@
 package com.dy.bromatchbackend.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.dy.bromatchbackend.service.UserService;
 import com.dy.bromatchbackend.model.domain.User;
 import com.dy.bromatchbackend.mapper.UserMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.regex.Pattern;
 
 /**
 * @author LEGION
@@ -14,6 +21,119 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService {
+    @Resource
+    private UserMapper userMapper;
+    /**
+     * 盐值：混淆密码
+     */
+    private static final String SALT = "dy";
+
+    @Override
+    public long userRegister(String userAccount, String userPassword, String checkPassword) {
+        //1. 校验
+        // 非空
+        if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)) {
+            return -1;
+//            throw new BusinessException(ErrorCode.PARAMS_ERROR,"请求参数不能为空");
+        }
+        //长度
+        if (userAccount.length() < 4) {
+            return -1;
+//            throw new BusinessException(ErrorCode.PARAMS_ERROR,"账号长度过短");
+        }
+        if(userPassword.length()<8 || checkPassword.length()<8){
+            return -1;
+//            throw new BusinessException(ErrorCode.PARAMS_ERROR,"密码长度过短");
+        }
+        //账户不能包含特殊字符
+        String pattern = ".*[*?!&￥$%^#,./@\";:><\\]\\[}{\\-=+_\\\\|》《。，、？’‘“”~`）].*$";
+        if(Pattern.matches(pattern, userAccount)) {
+            return -1;
+//            throw new BusinessException(ErrorCode.PARAMS_ERROR,"账号不能包含特殊字符");
+        }
+        //密码和校验密码相同
+        if(!userPassword.equals(checkPassword)){
+            return -1;
+//            throw new BusinessException(ErrorCode.PARAMS_ERROR,"密码和校验密码相同");
+        }
+        //账户不能重复 (查询了数据库，这个校验应该放到最后校验)
+        QueryWrapper<User> qw = new QueryWrapper<>();
+        qw.eq("userAccount",userAccount);
+        long count = this.count(qw);
+        if(count>0){
+            return -1;
+//            throw new BusinessException(ErrorCode.PARAMS_ERROR,"账号已注册");
+        }
+        //2.加密
+        String md5Password = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+        //3. 插入数据
+        User user = new User();
+        user.setUserAccount(userAccount);
+        user.setUserPassword(md5Password);
+        boolean result = this.save(user);//保存user成功后，会把id塞给user
+        if(!result){
+//            throw new BusinessException(ErrorCode.SYSTEM_EXCEPTION,"注册失败");
+            return -1;
+        }
+        return user.getId();
+    }
+
+    @Override
+    public User userLogin(String userAccount, String userPassword, HttpServletRequest request) {
+        //1. 校验
+        // 非空
+        if (StringUtils.isAllBlank(userAccount, userPassword)) {
+//            throw new BusinessException(ErrorCode.PARAMS_ERROR,"请求参数不能为空");
+        }
+        //长度
+        if (userAccount.length() < 4) {
+//            throw new BusinessException(ErrorCode.PARAMS_ERROR,"账号长度过短");
+        }
+        if(userPassword.length()<8){
+//            throw new BusinessException(ErrorCode.PARAMS_ERROR,"密码长度过短");
+        }
+        //账户不能包含特殊字符
+        String pattern = ".*[*?!&￥$%^#,./@\";:><\\]\\[}{\\-=+_\\\\|》《。，、？’‘“”~`）].*$";
+        if(Pattern.matches(pattern, userAccount)) {
+//            throw new BusinessException(ErrorCode.PARAMS_ERROR,"账号不能包含特殊字符");
+        }
+        //2.加密
+        String md5Password = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+        //查询用户是否存在
+        QueryWrapper<User> qw = new QueryWrapper<>();
+        qw.eq("userAccount",userAccount).eq("userPassword",md5Password);
+        User user = userMapper.selectOne(qw);
+        if (user == null) {
+//            log.info("user login failed, userAccount cannot match userPassword");
+//            throw new BusinessException(ErrorCode.PARAMS_ERROR,"账号或密码不正确");
+        }
+        //3.用户脱敏
+        User safetyUser = getSaftyUser(user);
+        //4.记录用户的登录态
+//        request.getSession().setAttribute(USER_LOGIN_STATE,safetyUser);
+        return safetyUser;
+    }
+
+    /**
+     * 用户脱敏
+     * @param user
+     * @return
+     */
+    @Override
+    public User getSaftyUser(User user) {
+        User safetyUser = new User();
+        safetyUser.setId(user.getId());
+        safetyUser.setUsername(user.getUsername());
+        safetyUser.setUserAccount(user.getUserAccount());
+        safetyUser.setAvatarUrl(user.getAvatarUrl());
+        safetyUser.setGender(user.getGender());
+        safetyUser.setPhone(user.getPhone());
+        safetyUser.setEmail(user.getEmail());
+        safetyUser.setUserStatus(user.getUserStatus());
+        safetyUser.setCreateTime(user.getCreateTime());
+        safetyUser.setUserRole(user.getUserRole());
+        return safetyUser;
+    }
 
 }
 
